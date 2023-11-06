6340fcbe1341b35c2759bda009158381424d21f0
Taras Pavlenko <taras.pavlenko95@gmail.com>
2023-11-06 16:24:50 +0200

    Add checking that product has added to the cart

--- tests/CustomerApp.spec.js
+++ tests/CustomerApp.spec.js
@@ -5,17 +5,21 @@ test.only('Login and checkout', async ({page}) =>
     const userEmail = page.locator("#userEmail");
     const userPassword = page.locator("#userPassword");
     const loginButton = page.locator("#login");
-    const productList = page.locator(".card-body")
-    
+    const productList = page.locator(".card-body");  
+
     const desiredProductName = 'iphone 13 pro';
+    const cartPage = page.locator('[routerlink*="cart"]');
 
 
     await page.goto('https://rahulshettyacademy.com/client');
     await userEmail.type('tpavlenko@mail.com');
     await userPassword.type('Qwerty1715');
     await loginButton.click();
-    await page.waitForLoadState('networkidle'); 
-    const count = await productList.count();      
+    await page.waitForLoadState('networkidle');     
+    const titles = await page.locator(".card-body b").allTextContents();
+    console.log(titles);
+    const count = await productList.count();  
+
     for (let i = 0; i < count; ++i) {
         if (await productList.nth(i).locator("b").textContent() === desiredProductName) {
             //adding to the cart
@@ -24,4 +28,11 @@ test.only('Login and checkout', async ({page}) =>
         }
     }
     
+
+    await cartPage.click();
+    await page.locator("div li").first().waitFor();
+    const cartChecking = await page.locator("h3:has-text('iphone 13 pro')").isVisible();
+    expect(cartChecking).toBeTruthy();
+    await page.pause();
+
 });
